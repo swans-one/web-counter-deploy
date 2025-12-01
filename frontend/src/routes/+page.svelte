1acea1f290304a179433050cff20196546a166d2
Erik Swanson <theerikswanson@gmail.com>
2025-12-01 14:43:54 -0500

    Main page basic styling

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col items-center py-24">
+  <h1 class="pb-8 font-bold text-4xl md:text-6xl lg:text-8xl">
+    A Web Counter!
+  </h1>
+  <p class="pb-4 text-2xl">
+    There have been
+  </p>
+  <div class="pb-4 text-4xl lg:text-8xl">
+    0
+  </div>
+  <p class="text-2xl">
+    visitors to this page
+  </p>
+</div>
