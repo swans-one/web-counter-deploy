b7c2ca47b3ca53f70e43048dfae883362d08df81
Erik Swanson <theerikswanson@gmail.com>
2026-01-06 13:11:20 -0500

    Add script to move unitfiles into place

---
+++ podman/quadlet-setup.sh
@@ -0,0 +1,13 @@
+set -euo pipefail
+IFS=$'\n\t'
+
+WORK_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
+UNITFILE_DEST_DIR="$HOME/.config/containers/systemd"
+
+# Create the directory for unitfiles if it doesn't exist
+mkdir -p $UNITFILE_DEST_DIR
+
+# Copy all the unit files into that directory
+cp "$WORK_DIR/quadlet/*" "$UNITFILE_DEST_DIR"
+
+systemctl --user daemon-reload
